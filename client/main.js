e196fded2d3fc3521eb9c373d9be31e3751b4c66
Xinyang <xinyzhang9@gmail.com>
2016-04-19 11:55:47 -0700

    css

--- client/main.js
+++ client/main.js
@@ -1,10 +1,22 @@
 import '../imports/ui/body.js';
-
+import { Girls } from '../imports/api/girls.js';
 Router.route('/',{
+	name: 'home',
 	template: 'home'
 })
-Router.route('/admin');
+Router.route('/admin',{
+	name: 'admin',
+    template: 'admin'
+});
 
 Router.configure({
     layoutTemplate: 'main'
+});
+
+Router.route('/member/:_id', {
+    template: 'memberProfile',
+    data: function(){
+        var member_id = this.params._id;
+        return Girls.findOne({ _id : member_id });
+    }
 });
